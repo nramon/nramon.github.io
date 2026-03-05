8abcfec5a69ed0d66d36a8bd9d2adb0d76b4162f
Ramon Novoa <ramonnovoa@gmail.com>
2026-03-05 12:07:00 +0100

    Add a CS book list and small improvements.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 // Pad hours, minutes or seconds to two digits.
 function pad(num) {
-    return num < 10 ? "0" + num : num;
+	return num < 10 ? "0" + num : num;
 }
 
 // Print the current date to the element #date.
 function printDate() {
-    d = new Date();
-    date_str = d.toDateString(); 
-    time_str = pad(d.getHours()) + ':' + pad(d.getMinutes()) + ':' + pad(d.getSeconds()) + ' ';
-    document.getElementById('date').innerHTML = date_str.substring(0, date_str.length - 4) + time_str + date_str.substring(date_str.length - 4);
+	const d = new Date();
+	const date_str = d.toDateString();
+	const time_str =
+		pad(d.getHours()) +
+		":" +
+		pad(d.getMinutes()) +
+		":" +
+		pad(d.getSeconds()) +
+		" ";
+	document.getElementById("date").innerHTML =
+		date_str.substring(0, date_str.length - 4) +
+		time_str +
+		date_str.substring(date_str.length - 4);
 }
